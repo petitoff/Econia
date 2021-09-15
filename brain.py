1e0a3d8b4f7b3a5d87c88fec5356a7e50232088b
petit <petitoff@outlook.com>
2021-09-15 16:23:17 +0200

    Moved all checking and control over the text to the brain class. Changing the record of information about what the user sent to the form of 3 keys.

--- brain.py
+++ brain.py
@@ -3,43 +3,12 @@ import json
 
 class Chat:
     def __init__(self):
-        self.msg_user = ""  # Variable holding the message sent by the user.
+        msg_user = ""  # Variable holding the message sent by the user.
         self.msg_bot = ""  # Variable holding the message the bot will send.
 
     def get_msg(self, msg):
-        self.msg_user = msg
-        self.msg_analyze()
+        run_brain.brain_main(msg)
         return self.msg_bot
-
-    def msg_analyze(self):
-        self.msg_text_unification()
-
-        run_brain.brain_main(self.msg_user)
-
-        self.msg_bot = self.msg_user
-        return
-
-    def msg_text_unification(self):
-        self.msg_user = self.msg_user.replace(" ", "")
-        self.msg_user = self.msg_user.lower()
-
-        dct_polish_special_characters = {
-            "ą": "a", "ć": "c", "ę": "e", "ł": "l",
-            "ń": "n", "ó": "o", "ś": "s", "ź": "z", "ż": "z"}
-
-        dct_special_characters = {"!", "?", ".", ",",
-                                  "<", ">", "/", ";", "(", ")", ":", '"', "'", ":"}
-
-        # Searching for Polish characters in the text and finding their equivalents in asci without Polish characters.
-        asci_table = self.msg_user.maketrans(dct_polish_special_characters)
-        # Translate characters from an ascii array to normal text.
-        translate_from_asci = self.msg_user.translate(asci_table)
-
-        # Removes characters from text if they are in the dictionary.
-        # translate_from_asci = translate_from_asci.translate(
-        #     {ord(i): None for i in dct_special_characters})
-
-        self.msg_user = translate_from_asci
 
 
 class Brain:
@@ -51,9 +20,16 @@ class Brain:
 
         self.cleaning_special_characters()
 
+        self.basic_search()
+
         print(self.data_msg_user_contex)
 
     def put_in_dct(self, user_msg):
+        self.data_msg_user_contex.append(
+            {"textMain": user_msg.replace(" ", "")})
+
+        user_msg = self.msg_text_unification(user_msg)
+
         if user_msg[-1] == "?":
             kind = "question"
         elif user_msg[-1] == "!":
@@ -63,10 +39,25 @@ class Brain:
         else:
             kind = None
 
-        self.data_msg_user_contex.append({"text": user_msg, "kind": kind})
+        self.data_msg_user_contex[-1].update(
+            {"textUni": user_msg, "kind": kind})
+
+    def msg_text_unification(self, user_msg):
+        user_msg = self.data_msg_user_contex[-1]["textMain"]
+        user_msg = user_msg.lower()
+
+        dct_polish_special_characters = {
+            "ą": "a", "ć": "c", "ę": "e", "ł": "l",
+            "ń": "n", "ó": "o", "ś": "s", "ź": "z", "ż": "z"}
+
+        # Searching for Polish characters in the text and finding their equivalents in asci without Polish characters.
+        asci_table = user_msg.maketrans(dct_polish_special_characters)
+        # Translate characters from an ascii array to normal text.
+        translate_from_asci = user_msg.translate(asci_table)
+        return translate_from_asci
 
     def cleaning_special_characters(self):
-        translate_from_asci = self.data_msg_user_contex[-1]["text"]
+        translate_from_asci = self.data_msg_user_contex[-1]["textUni"]
 
         dct_special_characters = {"!", "?", ".", ",",
                                   "<", ">", "/", ";", "(", ")", ":", '"', "'", ":"}
@@ -75,13 +66,13 @@ class Brain:
             {ord(i): None for i in dct_special_characters})
 
         self.data_msg_user_contex[-1].update(
-            {"text": translate_from_asci})
+            {"textUniSepcial": translate_from_asci})
 
     def context(self):
         pass
 
     def basic_search(self):
-        pass
+        selfmsg_user = self.data_msg_user_contex[-1]["textUniSepcial"]
 
 
 run_chat = Chat()
