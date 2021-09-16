07cc4634ee86cfe17242917b7541b9a458dfc3a6
petit <petitoff@outlook.com>
2021-09-16 15:27:21 +0200

    Removal of unnecessary lines of code.

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-import json
 from brain import run_chat
-
-# with open('memory/typespecific_responses.json', encoding="utf-8") as json_file:
-#     data = json.load(json_file)
-#     print(data["questions"]["casual1"])
 
 
 def test_get_msg_user():
